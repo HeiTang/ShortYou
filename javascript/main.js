0cc0dd083e0deaf93696496140fa43bff6744dd8
HeiTang <heitang512@purr.tw>
2021-08-07 20:59:06 +0800

    feat: reCAPTCHA

--- javascript/main.js
+++ javascript/main.js
@@ -19,10 +19,11 @@ if (hash) {
 let btn = document.getElementById('btn');
 let inp = document.getElementById('url');
 let ali = document.getElementById('alias');
+let rec = document.getElementById('recaptcha');
 let res = document.getElementById('result');
 
 inp.addEventListener('input', () => {
-    btn.style.display = ali.style.display = ((inp.value.startsWith("http")) ? "block" : "none");
+    rec.style.display = ali.style.display = ((inp.value.startsWith("http")) ? "block" : "none");
 });
 
 btn.addEventListener('click', () => {
@@ -30,6 +31,55 @@ btn.addEventListener('click', () => {
     $.post(api, { url: inp.value, alias: (ali.value == "" ? undefined : ali.value) }, data => { res.innerText = `${uri}#${data.result}`; elem.style.display = "none"; }, 'json');
 });
 
+// GET IP
+var uriIP = 'https://www.cloudflare.com/cdn-cgi/trace';
+var ip;
+fetch(uriIP)
+    .then(response => response.text())
+    .then(result => {
+        var resultArr = result.split('\n');
+        for (var i = 0, len = resultArr.length; i < len; i++) {
+            var tempArr = resultArr[i].split('=');
+            if (tempArr[0] == 'ip') {
+                ip = tempArr[1];
+                break;
+            }
+        }
+    })
+    .catch(err => {
+        window.alert(err)
+    });
+
+// reCAPTCHA
+
+function verifyCallback(token) {
+    var formData = new FormData();
+    formData.append('token', token);
+    formData.append('ip', ip);
+
+    // Google Apps Script 部署為網路應用程式後取得的 URL
+    var uriGAS = "https://script.google.com/macros/s/AKfycbymUjNyL3y34V1P1eAAS0Nli3jV7JxlLR-QYE-eKoXLMbKI3tTAHO4RI91vtyGD3Nke/exec";
+
+    fetch(uriGAS, {
+        method: "POST",
+        body: formData
+    }).then(response => response.json())
+        .then(result => {
+            if (result.success) {
+                // 後端驗證成功，success 會是 true
+                // 這邊寫驗證成功後要做的事
+                document.getElementById('btn').classList.remove('none');
+            } else {
+                // success 為 false 時，代表驗證失敗，error-codes 會告知原因
+                window.alert(result['error-codes'][0])
+            }
+        })
+        .catch(err => {
+            window.alert(err)
+        })
+}
+
+// Backspace
 
 var TxtRotate = function (el, toRotate, period) {
     this.toRotate = toRotate;
@@ -40,25 +90,25 @@ var TxtRotate = function (el, toRotate, period) {
     this.tick();
     this.isDeleting = false;
 };
-  
+
 TxtRotate.prototype.tick = function () {
     var i = this.loopNum % this.toRotate.length;
     var fullTxt = this.toRotate[i];
-  
+
     if (this.isDeleting) {
         this.txt = fullTxt.substring(0, this.txt.length - 1);
     } else {
         this.txt = fullTxt.substring(0, this.txt.length + 1);
     }
-  
+
     this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
-  
+
     var that = this;
     // var delta = 200 - Math.random() * 100;
-  
+
     var delta = 200;
     if (this.isDeleting) { delta /= 2; }
-  
+
     if (!this.isDeleting && this.txt === fullTxt) {
         delta = this.period;
         this.isDeleting = true;
@@ -67,12 +117,12 @@ TxtRotate.prototype.tick = function () {
         this.loopNum++;
         delta = 500;
     }
-  
+
     setTimeout(function () {
         that.tick();
     }, delta);
-  };
-  
+};
+
 window.onload = function () {
     var elements = document.getElementsByClassName('txt-rotate');
     for (var i = 0; i < elements.length; i++) {
@@ -87,6 +137,6 @@ window.onload = function () {
     css.type = "text/css";
     css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
     document.body.appendChild(css);
-  };
+};
 
 

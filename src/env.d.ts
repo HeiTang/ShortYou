2aa8494b9ee31b317553a567cdf847a800ee1742
HeiTang <heitang.me@gmail.com>
2026-05-14 03:13:30 +0800

    build(ts): decouple frontend and GAS TypeScript config

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="astro/client" />
+
+interface Window {
+	__SHORTYOU_CONFIG__: {
+		api: string;
+		recaptchaSiteKey?: string;
+	};
+	verifyCallback?: (token: string) => void;
+}
